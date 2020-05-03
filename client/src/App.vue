67872400915d1750c3621fecbd041c6ade007626
jeee <45247795+je888@users.noreply.github.com>
2020-05-03 11:47:05 -0400

    initialize page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/cat.jpeg">
+    <HelloWorld msg="Hello,this is 圆"/>
   </div>
 </template>
 
