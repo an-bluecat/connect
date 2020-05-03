43bbfd28a4f8d6c2d47bd182d313e93ed9ed9af3
jeee <45247795+je888@users.noreply.github.com>
2020-05-03 14:26:07 -0400

    update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/cat.jpeg">
-    <HelloWorld msg="Hello,this is 圆"/>
+    <Todos />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
+  },
+  data(){
+    return{
+      todos:[
+        {
+          id:1,
+          title:"todo1",
+          completed: false
+        },
+        {
+          id:1,
+          title:"Todo2",
+          completed: false
+        }
+
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  box-sizing:border-box;
+  margin: 0;
+  padding: 0;
+
+}
+
+body{
+  font-family:'Courier New', Courier, monospace;
+  line-height: 1.4;
 }
 </style>
---
+++ client/src/components/Todos.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Todos</h1>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name:"Todos"
+}
+</script>
+
+<style scoped>
+
+</style>
