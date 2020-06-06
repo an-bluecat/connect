abff9cbb5c4fb1743bf910c73c26698291dab295
jeee <45247795+je888@users.noreply.github.com>
2020-06-06 11:56:37 -0400

    add json server

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <HelloWorld></HelloWorld>
+    <Todos></Todos>
   </div>
 </template>
 
--- client/src/components/Todos.vue
+++ client/src/components/Todos.vue
@@ -1,14 +1,50 @@
 <template>
     <div>
         <h1>Todos</h1>
+        <input v-model="todoName" @keyup.enter="addTodo">
+        <ul>
+            <li v-for="todo of todos" :key="todo.id">
+                {{todo.name}}
+            </li>
+
+        </ul>
+
     </div>
     
 </template>
 
+
 <script>
+import axios from "axios";
+
+const baseURL="http://localhost:3000/todos";
+
+
 export default {
     name:"Todos",
-    props: ["todos"]
+    data(){
+        return {
+            todos: [],
+            todoName: ''
+        }
+    },
+    async created(){
+        try{
+            const res = await axios.get(baseURL);
+            this.todos=res.data;
+        }catch(e){
+            consol.error(e);
+        }
+    },
+    methods: {
+        async addTodo() {
+            const res = await axios.post(baseURL, { name: this.todoName })
+
+            this.todos = [...this.todos, res.data]
+
+            this.todoName = ''
+    }
+  }
 }
 </script>
 
