a0f1eec2fdd1e85ed2750d5246de0ceec18807f5
haohao1331 <haohao1331@gmail.com>
2020-08-08 11:01:12 -0400

    styled welcome page course list

---
+++ src/components/course/SeachCourse.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-autocomplete
+        v-model="model"
+        :items="items"
+        color="white"
+        hide-selected
+        item-text="Description"
+        item-value="Course"
+        label="Search your Courses"
+        placeholder="Start typing to Search"
+        prepend-icon="mdi-database-search"
+        
+        @change="onchangeclass()"
+    ></v-autocomplete>
+</template>
+
+<script>
+import courseimport from './courseimport.json'
+
+export default {
+    data: () => ({
+        model: null,
+        courses: courseimport
+    }),
+    methods: {
+        onchangeclass(){
+            if(this.model == null){
+                return
+            }
+            this.$router.push('./course/'+ this.model)
+        }
+    },
+    computed: {
+        items () {
+            var result = []
+            for(let num in this.courses){
+                result.push(this.courses[num]['name'])
+            }
+            console.log("result", result)
+            return result
+        },
+    },
+}
+</script>
+
+<style scoped>
+.v-autocomplete{
+    min-width: 500px;
+    min-height: 100px;
+}
+</style>
--- src/components/course/courseList.vue
+++ src/components/course/courseList.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <ul v-if= "displaySearch===false">
-                <li v-for="course of courses" :key="course.id">
-                    <router-link v-bind:to="{
-                        name: 'class-details',
-                        params:{name: course.name, id: course.id}
-                    }">
-                    {{course.name}}
-                    </router-link>
-                </li>
+            <li v-for="course of courses" :key="course.id">
+                <router-link v-bind:to="{
+                    name: 'class-details',
+                    params:{name: course.name, id: course.id}
+                }">
+                {{course.name}}
+                </router-link>
+            </li>
         </ul>
         <ul v-if= "displaySearch">
             <li v-for="course of searchedCourses" :key="course.id">
@@ -35,30 +35,6 @@ export default {
             displaySearch: false
         }
     },
-    // async created(){
-    // created(){
-    //     try{
-    //         // const res = await axios.get(baseURL);
-    //         // const res = axios.get(baseURL);
-    //         // this.courses=res.data;
-    //         this.courses=[
-    //           {
-    //             "name": "ECE110",
-    //             "id": 1
-    //           },
-    //           {
-    //             "name": "ECE243",
-    //             "id": 2
-    //           },
-    //           {
-    //             "name": "ECE297",
-    //             "id": 3
-    //           }
-    //         ]
-    //     }catch(e){
-    //         console.error(e);
-    //     }
-    // },
     methods: {
         searchCourse: function() {
             this.searchedCourses=[]
