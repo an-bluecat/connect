6c74b7bb6af146e544b251d86c598112fca6f67a
alissalozhkin <65978304+alissalozhkin@users.noreply.github.com>
2021-09-11 22:11:12 -0400

    Add files via upload

---
+++ src/components/course/coursecsv/easy_courses.js
@@ -0,0 +1,113 @@
+const data1 = [
+    {"breadth": "Breadth Requirements: Thought, Belief and Behaviour (2)", 
+    "desc": "This course introduces linguistic analysis with a view towards its application to the study of the relation between culture and social structure. The interplay of pronunciation, grammar, semantics, and discourse with rituals, ideologies, and constructions of social meaning and worldview are discussed in tandem with the traditional branches of linguistic analysisphonology, morphology, grammar, syntax, and semantics. The objective of the course is to provide a broad framework for understanding the role of language in society.", 
+    "title": "  ANT253H1 - Language & Society"},
+
+    {"breadth": "Breadth Requirements: Thought, Belief and Behaviour (2)", 
+    "desc": "An investigation of some pivotal periods in the history of science with an emphasis on the influences of philosophy on the scientists of the period, and the philosophical and social implications of the scientific knowledge, theory and methodology that emerged.", 
+    "title": "  HPS100H1 - Introduction to History and Philosophy of Science and Technology"},
+
+    {"breadth": "Breadth Requirements: The Physical and Mathematical Universes (5)", 
+    "desc": "The goal of this course is for students to become comfortable with abstraction, rigour, logic, and proofs. They will practice reading and understanding mathematical statements, analyzing definitions and properties, formulating conjectures and generalizations, providing and writing reasonable and precise arguments, writing and critiquing proofs. The instructor may use specific mathematical content, which could vary from year to year, to practice these skills. The course is aimed at students interested in the creative character of mathematics, particularly those planning to take any of our proof-oriented courses, and is an excellent preparation for MAT137Y1, MAT157Y1, or MAT240H1.  Note: students may take this course concurrently with MAT157Y1 or MAT137Y1, or prior to registering in MAT157Y1 or MAT137Y1. This course can also be used by students who have already taken MAT136H1 and wish to bridge the gap to MAT237Y1.", 
+    "title": "  MAT138H1 - Introduction to Proofs"},
+
+    {"breadth": "Breadth Requirements: The Physical and Mathematical Universes (5)", 
+    "desc": "Our place in the Universe. Phenomena we see in the sky. What we know about the Sun, the planets and comets, and the formation of the solar system \u2013 and how we know it. What makes planets suitable for life. Finding out about the nearest stars and their planets. This course is intended for students with no science or engineering background.", 
+    "title": "  AST101H1 - The Sun and Its Neighbours"},
+
+    {"breadth": "Breadth Requirements: Creative and Cultural Representations (1)", 
+    "desc": "An historical and systematic introduction to the main questions in the philosophy of art and beauty from Plato to the present. These include the relation between art and beauty, the nature of aesthetic experience, definitions and theories of art, the criteria of excellence in the arts, and the function of art criticism.", 
+    "title": "  PHL285H1 - Aesthetics"},
+
+    {"breadth": "Breadth Requirements: The Physical and Mathematical Universes (5)", 
+    "desc": "Humans have solved problems for millennia on computing devices by representing data as diverse numbers, text, images, sound and genomes, and then transforming the data. A gentle introduction to designing programs (recipes) for systematically solving problems that crop up in diverse domains such as science, literature, and graphics. Social and intellectual issues raised by computing. Algorithms, hardware, software, operating systems, the limits of computation. Note: you may not take this course concurrently with any Computer Science course, but you may take CSC108H1/CSC148H1 after CSC104H1.", 
+    "title": "  CSC104H1 - Computational Thinking"},
+
+    {"breadth": "Breadth Requirements: Society and its Institutions (3)", 
+    "desc": "Geography matters in the success of both public and private sector organizations. Using mostly retail examples contemporary location problems are addressed. The geographies of demand and supply are analyzed and trade area and site selection techniques are applied. The relevance of the planning context and utility of geovisualization techniques such as GIS are also briefly considered.", 
+    "title": "  GGR252H1 - Marketing Geography"},
+
+    {"breadth": "Breadth Requirements: Creative and Cultural Representations (1)", 
+    "desc": "An intensive basic course in spoken and written French for students who have no knowledge of French: comprehension, speaking, reading, and writing.", 
+    "title": "  FSL100H1 - French for Beginners"},
+
+    {"breadth": "Breadth Requirements: Living Things and Their Environment (4), Society and its Institutions (3)", 
+    "desc": "Society and culture from various anthropological perspectives: socio-cultural, evolutionary, archaeological, and linguistic. ", 
+    "title": "  ANT100Y1 - Introduction to Anthropology"},
+
+    {"breadth": "Breadth Requirements: Thought, Belief and Behaviour (2)", 
+    "desc": "An introduction to formal deductive logic. Semantics, symbolization, and techniques of natural deduction in sentential logic. Symbolization, natural deduction, and models in monadic predicate logic. Symbolization and natural deduction with polyadic predicates. Introduction to advanced concepts in first-order logic, such as operations, identity, and models.", 
+    "title": "  PHL245H1 - Modern Symbolic Logic"}
+
+]
+
+const data2 = [
+    {"breadth": "Breadth Requirements: Society and its Institutions (3)", 
+    "desc": "An investigation of the relationship between human beings and their natural and built environments. This interdisciplinary course will draw from the sciences, social sciences and the humanities to explore major social, cultural, economic, regulatory, ethical, ecological and technological aspects of environmental issues.", 
+    "title": "  ENV100H1 - Introduction to Environmental Studies"},
+
+    {"breadth": "Breadth Requirements: Thought, Belief and Behaviour (2)", 
+    "desc": "An introduction to issues in the fundamental branches of philosophy: metaphysics, which considers the overall framework of reality; epistemology, or the theory of knowledge; and related problems in the philosophy of science. Topics in metaphysics may include: mind and body, causality, space and time, God, freedom and determinism; topics in epistemology may include perception, evidence, belief, truth, skepticism.", 
+    "title": "  PHL232H1 - Knowledge and Reality"},
+
+    {"breadth": "Breadth Requirements: Thought, Belief and Behaviour (2)", 
+    "desc": "A brief introductory survey of psychology as both a biological and social science. Topics will include physiological, learning, perceptual, motivational, cognitive, developmental, personality, abnormal, and social psychology.", "title": "  PSY100H1 - Introductory Psychology"}
+    {"breadth": "Breadth Requirements: The Physical and Mathematical Universes (5)", "desc": "Scholarly discussion of the probability that there are planets with life elsewhere in the universe, from the perspective of current ideas concerning the origin and evolution of the universe, the solar system and life. Search techniques and possibilities for interstellar travel and space colonies are discussed.", 
+    "title": "  AST251H1 - Life on Other Worlds"},
+
+    {"breadth": "Breadth Requirements: The Physical and Mathematical Universes (5)", 
+    "desc": "The nature and evolution of the Earth; plate tectonics; rocks and minerals; volcanism; geological time; fossils; geology of Ontario; environmental issues; and human interactions with the planet.\u00a0 ESS105H1 is primarily intended as a science Distribution Requirement course for Humanities and Social Science students", 
+    "title": "  ESS105H1 - Our home planet"},
+
+    {"breadth": "Breadth Requirements: Creative and Cultural Representations (1)", 
+    "desc": "Social media, digital platforms, and networked technologies are deeply embedded in our contemporary lives, both shaping and shaped by users. But what are their social, cultural, political, and economic impacts? What consequences do they have for how we think, feel, socialize, work, play, and understand ourselves? Students in this course will explore, examine, and debate these questions with reference to their everyday experiences and current examples.", 
+    "title": "  BMS316H1 - Social Media & Digital Platforms"},
+
+    {"breadth": "Breadth Requirements: Society and its Institutions (3)", 
+    "desc": "Environmental design, in the context of this course, refers to design strategies that account for the ability of supporting ecosystems to continue to meet human needs and those of other lifeforms without diminishing biological diversity or environmental quality. This course takes a hands-on approach to investigating several environmental design issues: climate-responsive design, energy consumption, health and comfort, natural lighting and ventilation, and water management. Students will build up a design of a net-zero carbon residential building through several instructive design exercises during the semester, including hands-on measurement and calculation activities.", 
+    "title": "  ENV335H1 - Environmental Design"},
+
+    {"breadth": "Breadth Requirements: Society and its Institutions (3)", 
+    "desc": "Global forest resources; relationships between societies and forests, consumption, trade and valuation of timber and non-timber products; ecosystem services, climate change and forestry, tropical deforestation and softwood lumber dispute.", 
+    "title": "  FOR303H1 - Human Dimensions of Global Forests"},
+
+    {"breadth": "Breadth Requirements: Creative and Cultural Representations (1)", 
+    "desc": "A survey of the history of art, architecture and allied arts. This introduction to the history of art will examine a wide range of objects, selected and discussed in connection with a special theme to be selected by the instructor. Students will be expected to study the history and significance of art through the close reading of selected texts that relate to both art and theory. Special attention will be given to developing essential art historical skills necessary for upper level courses. The topic for each semester will vary based on the instructor. No previous knowledge of history of art or architecture is required. Frequent writing assignments and exercises will be based on readings, lectures, and museum or gallery visits to collections in Toronto.", 
+    "title": "  FAH102H1 - Art and Ideas"},
+
+    {"breadth": "Breadth Requirements: Society and its Institutions (3)", 
+    "desc": "Offers an introduction to North American cities and urbanization in a global context. It explores social, cultural, political and economic forces, processes, and events that shape contemporary urbanism. The course adopts the lens of 'fixity' and 'flow' to examine how the movement of people, ideas, goods, and capital, as well as their containment in the infrastructure and space of the city, give rise to particular urban forms.", 
+    "title": "  GGR124H1 - Cities and Urban Life"}
+
+]
+
+const data3 = [
+    {"breadth": "Breadth Requirements: Thought, Belief and Behaviour (2)", 
+    "desc": "An introduction to some of the central problems of philosophy. Examples of questions that may be considered include: What is sound reasoning? What can we know? What is ultimately real? Is morality rational? Do humans have free will? Is there a God? What is consciousness? Should we fear death? What is justice?", 
+    "title": "  PHL101Y1 - Introduction to Philosophy (Thematic)"},
+
+    {"breadth": "Breadth Requirements: Society and its Institutions (3)", 
+    "desc": "Examine the history, lands, peoples, religions, literatures and cultures that produced the Hebrew Bible/Old Testament. Topics to be covered include an overview of the geography and history of Ancient Israel and Judea, the role of the Hebrew, Aramaic, and Greek languages, the literary genres reflected in biblical and some contemporary non-biblical texts, and the scholarly methods by which the Bible is studied.", 
+    "title": "  NMC104H1 - The Biblical World"},
+
+    {"breadth": "Breadth Requirements: Living Things and Their Environment (4)", 
+    "desc": "This course introduces students to the processes and strategies for discovering new drugs with a special focus on current and emerging approaches for the rational design of drugs that are both effective and safe.", 
+    "title": "  PCL102H1 - The Art of Drug Discovery"},
+
+    {"breadth": "Breadth Requirements: Living Things and Their Environment (4)", 
+    "desc": "Evolution and adaptation of life on Earth. Introduction to the theory of evolution by natural selection. Topics may include: evidence supporting the fact of evolution, and how evolutionary theory can help explain the world around us, such as how species are formed, and the evolution of sex, infanticide, and disease. For non-science students in all years and disciplines. For non-science students in all years and disciplines.", 
+    "title": "  EEB214H1 - Evolution and Adaptation"},
+
+    {"breadth": "Breadth Requirements: The Physical and Mathematical Universes (5)", 
+    "desc": "What we know about the properties and life cycles of stars, of galaxies, and of the Universe itself and how we know it. How astronomers develop methods for understanding phenomena that span such vast ranges in distance and time. This course is intended for students with no science or engineering background.", 
+    "title": "  AST201H1 - Stars and Galaxies"},
+    
+    {"breadth": "Breadth Requirements: Creative and Cultural Representations (1)", 
+    "desc": "This course teaches students who already write effectively how to write clear, compelling, research-informed English essays. The course aims to help students recognize the function of grammar and rhetoric, the importance of audience, and the persuasive role of style.", 
+    "title": "  JWE206H1 - Writing English Essays"},
+
+    {"breadth": "Breadth Requirements: Thought, Belief and Behaviour (2)", 
+    "desc": "The study of technical and scientific terms derived from Latin and Greek: word elements, formation, analysis. The course is designed to give students in any field of specialization a better grasp of the derivation and basic meaning of English words derived from Latin and Greek elements.", 
+    "title": "  CLA201H1 - Latin & Greek in Scientific Terminology"}
+
+]
